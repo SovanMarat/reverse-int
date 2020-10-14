a724d893b9cc02f526cd37700a97b8b15fae226c
SovanMarat <maratwp@mail.ru>
2020-10-14 17:23:35 +0300

    feat: function reverse done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(n);
+  let str = String(num);
+  let arr = str.split('');
+  let arrReverse = arr.reverse().join('');
+  return Number(arrReverse);
 }
